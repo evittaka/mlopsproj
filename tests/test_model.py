a1c83a2d8bec94452da7a9e295b2f72746b6170a
evittaka <e.karietaite@gmail.com>
2025-01-13 11:26:54 +0100

    test 4

--- tests/test_model.py
+++ tests/test_model.py
@@ -8,13 +8,6 @@ def test_model():
     y = model(x)
     assert y.shape == (1, 10)
 
-def test_error_on_wrong_shape():
-    model = MyAwesomeModel()
-    with pytest.raises(ValueError, match='Expected input to a 4D tensor'):
-        model(torch.randn(1,2,3))
-    with pytest.raises(ValueError, match='Expected each sample to have shape [1, 28, 28]'):
-        model(torch.randn(1,1,28,29))
-
 @pytest.mark.parametrize("batch_size", [32, 64])
 def test_model(batch_size: int) -> None:
     model = MyAwesomeModel()
